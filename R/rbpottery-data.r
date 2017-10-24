429c14a1489b6fe6487ed7a8d7577cfb603fc489
chvrngit <33031672+chvrngit@users.noreply.github.com>
2017-10-24 16:38:16 -0600

    Add files via upload

---
+++ R/rbpottery-data.r
@@ -0,0 +1,14 @@
+#'Romano-British Pottery for wmpvaer
+#'
+#'Results of chemical analyses of 48 specimens of Romano-British pottery published by Tubb, et al. (1980).
+#'The numbers are the percentage metal oxide. "Kiln" indicates at which kiln site the pottery was found.
+#'The kiln sites come from three regions (1=Gloucester, (2=Llanedeyrn, 3=Caldicot), (4=Islands Thorns, 5=Ashley Rails)). 
+#'The data were scanned from Table 2.2 in Baxter (2003, p. 21)
+#' @docType data
+#' @usage data(rbpottery)
+#' @format typeof(rbpottery)=="list"
+#' @keywords datasets
+#' @references Baxter, M. J. 2003. Statistics in Archaeology. Arnold, London ,and,Tubb, A., A. J. Parker, 
+#'             and G. Nickless. 1980. The Analysis of Romano-British Pottery by Atomic Absorption Spectrophotometry. 
+#'             Archaeometry 22: 153-71.
+ "rbpottery"
---
+++ R/wmpvaer.r
@@ -0,0 +1,12 @@
+#' wmpvaer: Wilks MANOVA p-Value Alternative Estimation
+#'
+#' The wmpvaer package provides four exported functions:
+#' wmpvaerf, mlandrf,statsBIf,statBIncatf,statsECMf
+#' 
+#' @docType package 
+#' @name wmpvaerf
+#' @name mlandrf
+#' @name statsBIf 
+#' @name statsBIncatf
+#' @name statsECMf
+NULL 
